d835f8a63c1ad5f4b885e1c749af9168ebf1451d
bert093 <sas992203@gmail.com>
2024-06-22 18:14:28 +0800

    menambahkan keterangan!

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_tugas/regis_login02/register02.dart';
+import 'package:flutter/material.dart'; // memanggil library flutter
+import 'package:flutter_tugas/regis_login02/register02.dart'; // memanggil file register02.dart
 
-void main() {
-  runApp(const MainApp());
+void main() { 
+  runApp(const MainApp()); // memanggil class MainApp
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatelessWidget { // membuat class MainApp yang merupakan widget stateless
+  const MainApp({super.key}); // konstruktor dari class MainApp
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Register02(),
+  Widget build(BuildContext context) { // method build digunakan untuk membuat widget
+    return const MaterialApp( 
+      home: Register02(), // home digunakan untuk menentukan halaman utama
       // home: Scaffold(
       //   body: Center(
       //     child: Text('Hello World!'),
--- lib/regis_login02/register02.dart
+++ lib/regis_login02/register02.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tugas/regis_login02/login02.dart';
 
-class Register02 extends StatefulWidget {
-  const Register02({super.key});
+class Register02 extends StatefulWidget { // class Register02 merupakan stateful widget yang berarti class ini memiliki state yang dapat berubah
+  const Register02({super.key}); // konstruktor dari class Register02
 
   @override
-  _UserProfileFormState createState() => _UserProfileFormState();
+  _UserProfileFormState createState() => _UserProfileFormState(); // Anotasi @override digunakan untuk menunjukkan bahwa metode createState ini mengoverride metode yang ada di kelas induk (StatefulWidget). Ini adalah cara untuk memastikan bahwa Anda benar-benar mengoverride metode yang ada dan bukan secara tidak sengaja membuat metode baru.
 }
 
-class _UserProfileFormState extends State<Register02> {
-  // Controllers for text fields
-  final TextEditingController _namaController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _tlpnController = TextEditingController();
-  final TextEditingController _pswdController = TextEditingController();
+class _UserProfileFormState extends State<Register02> { // class _UserProfileFormState merupakan state dari class Register02
+  // Controllers for text fields (pak made)
+  final TextEditingController _namaController = TextEditingController(); // controller untuk text field nama
+  final TextEditingController _emailController = TextEditingController(); // controller untuk text field email
+  final TextEditingController _tlpnController = TextEditingController(); // controller untuk text field telepon
+  final TextEditingController _pswdController = TextEditingController(); // controller untuk text field password
 
-  // Variables for other inputs
-  bool _setuju = false;
-  String _jenisKelamin = '';
-  String _pilihKota = '<pilih>';
+  // Variables for other inputs (pak made)
+  bool _setuju = false; // variabel untuk checkbox persetujuan
+  String _jenisKelamin = ''; // variabel untuk radio button jenis kelamin
+  String _pilihKota = '<pilih>'; // variabel untuk dropdown kota terdekat
 
   @override
-  void dispose() {
-    // Clean up the controllers when the widget is disposed
+  void dispose() { // method dispose digunakan untuk membersihkan resource yang digunakan oleh widget
+    // dispose dipanggil ketika widget dihapus dari widget tree.
+    // Clean up the controllers when the widget is disposed (pak made)
     _namaController.dispose();
     _emailController.dispose();
     _tlpnController.dispose();
@@ -30,24 +31,24 @@ class _UserProfileFormState extends State<Register02> {
     super.dispose();
   }
 
-  void _register() {
-    String nama = _namaController.text;
-    String email = _emailController.text;
-    String password = _pswdController.text;
+  void _register() { // Metode _register() dalam kode Anda digunakan untuk menangani logika pendaftaran pengguna.
+    String nama = _namaController.text; // mengambil nilai dari text field nama
+    String email = _emailController.text; // mengambil nilai dari text field email
+    String password = _pswdController.text; // mengambil nilai dari text field password
 
-    if (email == '' || password == '' || _setuju == false) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Kesalahan !!'),
+    if (email == '' || password == '' || _setuju == false) { // jika email, password atau persetujuan kosong, maka akan menampilkan pesan dialog
+      showDialog( // method showDialog digunakan untuk menampilkan dialog
+        context: context, // context digunakan untuk mengakses konteks widget
+        builder: (BuildContext context) { // builder digunakan untuk membuat konten dari dialog
+          return AlertDialog( // AlertDialog digunakan untuk menampilkan dialog
+            title: Text('Kesalahan !!'), // title digunakan untuk judul dari dialog
             content:
                 Text('email, password atau persetujuan tidak boleh kosong'),
-            actions: <Widget>[
-              TextButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
+            actions: <Widget>[ // actions digunakan untuk tombol dalam dialog
+              TextButton( // TextButton digunakan untuk menampilkan tombol
+                child: Text('OK'), // child digunakan untuk konten dari tombol
+                onPressed: () { // onPressed digunakan untuk menangani klik tombol
+                  Navigator.of(context).pop(); // method pop digunakan untuk menutup dialog
                 },
               ),
             ],
@@ -55,14 +56,14 @@ class _UserProfileFormState extends State<Register02> {
         },
       );
     } else {
-      // Navigator.pushNamed dengan arguments untuk mengirim data ke halaman login
+      // Navigator.pushNamed dengan arguments untuk mengirim data ke halaman login (pak made)
       Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => LoginPage02(
-            registeredNama02: nama,
-            registeredEmail02: email,
-            registeredPassword02: password,
+            registeredNama02: nama, // mengirim data nama ke halaman login
+            registeredEmail02: email, // mengirim data email ke halaman login
+            registeredPassword02: password, // mengirim data password ke halaman login
           ),
         ),
       );
@@ -79,12 +80,12 @@ class _UserProfileFormState extends State<Register02> {
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
-            // Nama input
+            // Nama input (pak made)
             TextField(
               controller: _namaController,
               decoration: InputDecoration(labelText: 'Nama'),
             ),
-            // Email input
+            // Email input (pak made)
             TextField(
               controller: _emailController,
               decoration: InputDecoration(labelText: 'Email'),
@@ -96,14 +97,14 @@ class _UserProfileFormState extends State<Register02> {
               keyboardType: TextInputType.visiblePassword,
               obscureText: true,
             ),
-            // Telepon input
+            // Telepon input (pak made)
             TextField(
               controller: _tlpnController,
               decoration: InputDecoration(labelText: 'Telepon'),
               keyboardType: TextInputType.phone,
             ),
             SizedBox(height: 20),
-            // Jenis Kelamin selection
+            // Jenis Kelamin selection (pak made)
             Text('Jenis Kelamin',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             ListTile(
@@ -130,9 +131,9 @@ class _UserProfileFormState extends State<Register02> {
                 },
               ),
             ),
-            //Radio button Jenis Kelamin jangan ditambah lagi Pesan: MdYd.
+            //Radio button Jenis Kelamin jangan ditambah lagi Pesan: MdYd. (pak made)
             const SizedBox(height: 20),
-            // Kota Terdekat selection
+            // Kota Terdekat selection (pak made)
             const Text('Kota Terdekat',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             DropdownButton<String>(
@@ -142,7 +143,7 @@ class _UserProfileFormState extends State<Register02> {
                   _pilihKota = newValue!;
                 });
               },
-              items: <String>[
+              items: <String>[ // items digunakan untuk item dalam dropdown
                 '<pilih>',
                 'Mataram',
                 'Peraya',
@@ -160,7 +161,7 @@ class _UserProfileFormState extends State<Register02> {
               }).toList(),
             ),
             const SizedBox(height: 20),
-            // Subscription checkbox
+            // Subscription checkbox (pak made)
             CheckboxListTile(
               title: const Text('Saya setuju dengan peraturan yang berlaku'),
               value: _setuju,
@@ -171,18 +172,18 @@ class _UserProfileFormState extends State<Register02> {
               },
             ),
             SizedBox(height: 20),
-            // Submit button
+            // Submit button (pak made)
             ElevatedButton(
               onPressed: () {
                 _register();
-                // Handle form submission
-                String nama = _namaController.text;
-                String email = _emailController.text;
-                // String password = _pswdController.text;
-                String tlpn = _tlpnController.text;
-                String jenisKel = _jenisKelamin;
-                String kota = _pilihKota;
-                bool setuju = _setuju;
+                // Handle form submission (pak made)
+                String nama = _namaController.text; // mengambil nilai dari text field nama
+                String email = _emailController.text; // mengambil nilai dari text field email
+                // String password = _pswdController.text; (pak made)
+                String tlpn = _tlpnController.text; // mengambil nilai dari text field telepon
+                String jenisKel = _jenisKelamin; // mengambil nilai dari radio button jenis kelamin
+                String kota = _pilihKota; // mengambil nilai dari dropdown kota terdekat
+                bool setuju = _setuju; // mengambil nilai dari checkbox persetujuan
 
                 // tambahakan method untuk pindah ke form login 02
                 // Navigator.push(
