1aca857843916c9264d698db0e73b7ab5950a551
bert093 <sas992203@gmail.com>
2024-06-26 07:00:26 +0800

    add explanations/descriptions in the databarang and registers files

--- lib/regis_login02/data_barang02.dart
+++ lib/regis_login02/data_barang02.dart
@@ -1,87 +1,87 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // memanggil library flutter
 
-class Barang {
-  final String name;
-  final String image;
+class Barang { // membuat class Barang
+  final String name; // mendeklarasikan variabel name dengan tipe data String
+  final String image; // mendeklarasikan variabel image dengan tipe data String
 
-  Barang({required this.name, required this.image});
+  Barang({required this.name, required this.image}); // konstruktor dari class Barang
 }
 
-class BarangPage02 extends StatefulWidget {
-  const BarangPage02({super.key});
+class BarangPage02 extends StatefulWidget { // membuat class BarangPage02 yang merupakan widget stateful
+  const BarangPage02({super.key}); // konstruktor dari class BarangPage02
 
   @override
-  _BarangPageState createState() => _BarangPageState();
+  _BarangPageState createState() => _BarangPageState(); // method untuk membuat state dari BarangPage02
 }
 
-class _BarangPageState extends State<BarangPage02> {
-  final List<Barang> _items = [];
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _imageController = TextEditingController();
+class _BarangPageState extends State<BarangPage02> { // membuat class _BarangPageState yang merupakan state dari BarangPage02
+  final List<Barang> _items = []; // mendeklarasikan list _items dengan tipe data Barang
+  final TextEditingController _nameController = TextEditingController(); // mendeklarasikan controller untuk nama
+  final TextEditingController _imageController = TextEditingController(); // mendeklarasikan controller untuk image
 
-  void _addItem() {
-    final String name = _nameController.text;
-    final String image = _imageController.text;
+  void _addItem() { // method untuk menambahkan item
+    final String name = _nameController.text; // mengambil nilai name dari controller
+    final String image = _imageController.text; // mengambil nilai image dari controller
 
-    if (name.isNotEmpty && image.isNotEmpty) {
-      setState(() {
-        _items.add(Barang(name: name, image: image));
+    if (name.isNotEmpty && image.isNotEmpty) { // pengecekan jika name dan image tidak kosong
+      setState(() { // memperbarui state
+        _items.add(Barang(name: name, image: image)); // menambahkan item ke list _items
       });
-      _nameController.clear();
-      _imageController.clear();
+      _nameController.clear(); // membersihkan controller name
+      _imageController.clear(); // membersihkan controller image
     }
   }
 
   @override
-  void dispose() {
-    _nameController.dispose();
-    _imageController.dispose();
-    super.dispose();
+  void dispose() { // method dispose digunakan untuk membersihkan resource yang digunakan oleh widget
+    _nameController.dispose(); // membersihkan controller name
+    _imageController.dispose(); // membersihkan controller image
+    super.dispose(); // memanggil method dispose dari parent class
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Form Input Data Barang'),
+  Widget build(BuildContext context) { // method build untuk membuat widget
+    return Scaffold( // membuat scaffold
+      appBar: AppBar( // membuat app bar
+        title: Text('Form Input Data Barang'), // judul app bar
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
+      body: Padding( // padding untuk body
+        padding: const EdgeInsets.all(16.0), // padding 16.0
+        child: Column( // membuat column
           children: [
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: 'Nama Barang',
-                border: OutlineInputBorder(),
+            TextField( // membuat text field untuk nama
+              controller: _nameController, // controller untuk nama
+              decoration: InputDecoration( // dekorasi input
+                labelText: 'Nama Barang', // label nama barang
+                border: OutlineInputBorder(), // border outline
               ),
             ),
-            SizedBox(height: 10),
-            TextField(
-              controller: _imageController,
-              decoration: InputDecoration(
-                labelText: 'URL Gambar Barang',
-                border: OutlineInputBorder(),
+            SizedBox(height: 10), // jarak 10
+            TextField( // membuat text field untuk image
+              controller: _imageController, // controller untuk image
+              decoration: InputDecoration( // dekorasi input
+                labelText: 'URL Gambar Barang', // label URL gambar barang
+                border: OutlineInputBorder(), // border outline
               ),
             ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _addItem,
-              child: Text('Tambahkan Barang'),
+            SizedBox(height: 10), // jarak 10
+            ElevatedButton( // membuat tombol untuk menambahkan barang
+              onPressed: _addItem, // aksi tombol untuk menambahkan barang
+              child: Text('Tambahkan Barang'), // teks tombol
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _items.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      leading: Image.network(
-                        _items[index].image,
-                        fit: BoxFit.cover,
-                        width: 50,
-                        height: 50,
+            Expanded( // membuat list view 
+              child: ListView.builder( // membuat list view builder
+                itemCount: _items.length, // jumlah item
+                itemBuilder: (context, index) { // builder untuk item
+                  return Card( // membuat card
+                    child: ListTile( // membuat list tile
+                      leading: Image.network( // membuat image network
+                        _items[index].image, // URL gambar
+                        fit: BoxFit.cover, // menyesuaikan gambar
+                        width: 50, // lebar gambar
+                        height: 50, // tinggi gambar
                       ),
-                      title: Text(_items[index].name),
+                      title: Text(_items[index].name), // judul item
                     ),
                   );
                 },
--- lib/regis_login02/register02.dart
+++ lib/regis_login02/register02.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_tugas/regis_login02/login02.dart';
+import 'package:flutter/material.dart'; // memanggil library flutter
+import 'package:flutter_tugas/regis_login02/login02.dart'; // memanggil file login02.dart
 
 class Register02 extends StatefulWidget { // class Register02 merupakan stateful widget yang berarti class ini memiliki state yang dapat berubah
   const Register02({super.key}); // konstruktor dari class Register02
@@ -24,11 +24,11 @@ class _UserProfileFormState extends State<Register02> { // class _UserProfileFor
   void dispose() { // method dispose digunakan untuk membersihkan resource yang digunakan oleh widget
     // dispose dipanggil ketika widget dihapus dari widget tree.
     // Clean up the controllers when the widget is disposed (pak made)
-    _namaController.dispose();
-    _emailController.dispose();
-    _tlpnController.dispose();
-    _pswdController.dispose();
-    super.dispose();
+    _namaController.dispose(); // membersihkan controller nama
+    _emailController.dispose(); // membersihkan controller email
+    _tlpnController.dispose(); // membersihkan controller telepon
+    _pswdController.dispose(); // membersihkan controller password
+    super.dispose(); // memanggil method dispose dari parent class
   }
 
   void _register() { // Metode _register() dalam kode Anda digunakan untuk menangani logika pendaftaran pengguna.
@@ -43,7 +43,7 @@ class _UserProfileFormState extends State<Register02> { // class _UserProfileFor
           return AlertDialog( // AlertDialog digunakan untuk menampilkan dialog
             title: Text('Kesalahan !!'), // title digunakan untuk judul dari dialog
             content:
-                Text('email, password atau persetujuan tidak boleh kosong'),
+                Text('email, password atau persetujuan tidak boleh kosong'), // content digunakan untuk isi dari dialog
             actions: <Widget>[ // actions digunakan untuk tombol dalam dialog
               TextButton( // TextButton digunakan untuk menampilkan tombol
                 child: Text('OK'), // child digunakan untuk konten dari tombol
@@ -57,10 +57,10 @@ class _UserProfileFormState extends State<Register02> { // class _UserProfileFor
       );
     } else {
       // Navigator.pushNamed dengan arguments untuk mengirim data ke halaman login (pak made)
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => LoginPage02(
+      Navigator.push( // method push digunakan untuk navigasi ke halaman lain
+        context, // context digunakan untuk mengakses konteks widget
+        MaterialPageRoute( // MaterialPageRoute digunakan untuk membuat route baru
+          builder: (context) => LoginPage02( // builder digunakan untuk membuat konten dari route
             registeredNama02: nama, // mengirim data nama ke halaman login
             registeredEmail02: email, // mengirim data email ke halaman login
             registeredPassword02: password, // mengirim data password ke halaman login
@@ -71,76 +71,76 @@ class _UserProfileFormState extends State<Register02> { // class _UserProfileFor
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Form Pendaftaran'),
+  Widget build(BuildContext context) { // method build digunakan untuk membuat widget
+    return Scaffold( // Scaffold digunakan untuk membuat struktur dasar dari halaman
+      appBar: AppBar( // AppBar digunakan untuk membuat app bar
+        title: Text('Form Pendaftaran'), // title digunakan untuk judul dari app bar
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
+      body: Padding( // Padding digunakan untuk memberikan padding pada konten
+        padding: const EdgeInsets.all(16.0), // padding digunakan untuk memberikan jarak antara konten dengan tepi
+        child: ListView( // ListView digunakan untuk membuat daftar yang dapat di-scroll
           children: [
             // Nama input (pak made)
-            TextField(
-              controller: _namaController,
-              decoration: InputDecoration(labelText: 'Nama'),
+            TextField( // TextField digunakan untuk membuat input teks
+              controller: _namaController, // controller digunakan untuk mengontrol nilai dari input teks
+              decoration: InputDecoration(labelText: 'Nama'), // decoration digunakan untuk memberikan dekorasi pada input teks
             ),
             // Email input (pak made)
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
+            TextField( // TextField digunakan untuk membuat input teks
+              controller: _emailController, // controller digunakan untuk mengontrol nilai dari input teks
+              decoration: InputDecoration(labelText: 'Email'), // decoration digunakan untuk memberikan dekorasi pada input teks
+              keyboardType: TextInputType.emailAddress, // keyboardType digunakan untuk menentukan jenis keyboard yang muncul
             ),
-            TextField(
-              controller: _pswdController,
-              decoration: InputDecoration(labelText: 'Password'),
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
+            TextField( // TextField digunakan untuk membuat input teks
+              controller: _pswdController, // controller digunakan untuk mengontrol nilai dari input teks
+              decoration: InputDecoration(labelText: 'Password'), // decoration digunakan untuk memberikan dekorasi pada input teks
+              keyboardType: TextInputType.visiblePassword, // keyboardType digunakan untuk menentukan jenis keyboard yang muncul
+              obscureText: true, // obscureText digunakan untuk menyembunyikan teks yang diinput
             ),
             // Telepon input (pak made)
-            TextField(
-              controller: _tlpnController,
-              decoration: InputDecoration(labelText: 'Telepon'),
-              keyboardType: TextInputType.phone,
+            TextField( // TextField digunakan untuk membuat input teks
+              controller: _tlpnController, // controller digunakan untuk mengontrol nilai dari input teks
+              decoration: InputDecoration(labelText: 'Telepon'), // decoration digunakan untuk memberikan dekorasi pada input teks
+              keyboardType: TextInputType.phone, // keyboardType digunakan untuk menentukan jenis keyboard yang muncul
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 20), // SizedBox digunakan untuk memberikan jarak antara widget
             // Jenis Kelamin selection (pak made)
             Text('Jenis Kelamin',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            ListTile(
-              title: const Text('Pria'),
-              leading: Radio<String>(
-                value: 'Pria',
-                groupValue: _jenisKelamin,
-                onChanged: (String? value) {
-                  setState(() {
-                    _jenisKelamin = value!;
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)), // Text digunakan untuk menampilkan teks
+            ListTile( // ListTile digunakan untuk membuat item dalam daftar
+              title: const Text('Pria'), // title digunakan untuk judul dari item
+              leading: Radio<String>( // leading digunakan untuk menampilkan widget di sebelah kiri item
+                value: 'Pria', // value digunakan untuk nilai dari radio button
+                groupValue: _jenisKelamin, // groupValue digunakan untuk menentukan grup dari radio button
+                onChanged: (String? value) { // onChanged digunakan untuk menangani perubahan nilai
+                  setState(() { // setState digunakan untuk memperbarui state
+                    _jenisKelamin = value!; // memperbarui nilai dari _jenisKelamin
                   });
                 },
               ),
             ),
-            ListTile(
-              title: const Text('Wanita'),
-              leading: Radio<String>(
-                value: 'Wanita',
-                groupValue: _jenisKelamin,
-                onChanged: (String? value) {
-                  setState(() {
-                    _jenisKelamin = value!;
+            ListTile( // ListTile digunakan untuk membuat item dalam daftar
+              title: const Text('Wanita'), // title digunakan untuk judul dari item
+              leading: Radio<String>( // leading digunakan untuk menampilkan widget di sebelah kiri item
+                value: 'Wanita', // value digunakan untuk nilai dari radio button
+                groupValue: _jenisKelamin, // groupValue digunakan untuk menentukan grup dari radio button
+                onChanged: (String? value) { // onChanged digunakan untuk menangani perubahan nilai
+                  setState(() { // setState digunakan untuk memperbarui state
+                    _jenisKelamin = value!; // memperbarui nilai dari _jenisKelamin
                   });
                 },
               ),
             ),
             //Radio button Jenis Kelamin jangan ditambah lagi Pesan: MdYd. (pak made)
-            const SizedBox(height: 20),
+            const SizedBox(height: 20), // SizedBox digunakan untuk memberikan jarak antara widget
             // Kota Terdekat selection (pak made)
             const Text('Kota Terdekat',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            DropdownButton<String>(
-              value: _pilihKota,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _pilihKota = newValue!;
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)), // Text digunakan untuk menampilkan teks
+            DropdownButton<String>( // DropdownButton digunakan untuk membuat dropdown
+              value: _pilihKota, // value digunakan untuk nilai dari dropdown
+              onChanged: (String? newValue) { // onChanged digunakan untuk menangani perubahan nilai
+                setState(() { // setState digunakan untuk memperbarui state
+                  _pilihKota = newValue!; // memperbarui nilai dari _pilihKota
                 });
               },
               items: <String>[ // items digunakan untuk item dalam dropdown
@@ -153,29 +153,29 @@ class _UserProfileFormState extends State<Register02> { // class _UserProfileFor
                 'Dompu',
                 'Bima',
                 'Diluar NTB'
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
+              ].map<DropdownMenuItem<String>>((String value) { // map digunakan untuk mengubah item menjadi DropdownMenuItem
+                return DropdownMenuItem<String>( // DropdownMenuItem digunakan untuk item dalam dropdown
+                  value: value, // value digunakan untuk nilai dari item
+                  child: Text(value), // child digunakan untuk konten dari item
                 );
-              }).toList(),
+              }).toList(), // toList digunakan untuk mengubah iterable menjadi list
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 20), // SizedBox digunakan untuk memberikan jarak antara widget
             // Subscription checkbox (pak made)
-            CheckboxListTile(
-              title: const Text('Saya setuju dengan peraturan yang berlaku'),
-              value: _setuju,
-              onChanged: (bool? value) {
-                setState(() {
-                  _setuju = value!;
+            CheckboxListTile( // CheckboxListTile digunakan untuk membuat item dengan checkbox
+              title: const Text('Saya setuju dengan peraturan yang berlaku'), // title digunakan untuk judul dari item
+              value: _setuju, // value digunakan untuk nilai dari checkbox
+              onChanged: (bool? value) { // onChanged digunakan untuk menangani perubahan nilai
+                setState(() { // setState digunakan untuk memperbarui state
+                  _setuju = value!; // memperbarui nilai dari _setuju
                 });
               },
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 20), // SizedBox digunakan untuk memberikan jarak antara widget
             // Submit button (pak made)
-            ElevatedButton(
-              onPressed: () {
-                _register();
+            ElevatedButton( // ElevatedButton digunakan untuk membuat tombol
+              onPressed: () { // onPressed digunakan untuk menangani klik tombol
+                _register(); // memanggil method _register
                 // Handle form submission (pak made)
                 String nama = _namaController.text; // mengambil nilai dari text field nama
                 String email = _emailController.text; // mengambil nilai dari text field email
@@ -196,12 +196,12 @@ class _UserProfileFormState extends State<Register02> { // class _UserProfileFor
                 //   ),
                 // );
                 // data yang dimasukan, akan dicetak pada terminal untuk pengecekan
-                print('Nama: $nama');
-                print('Email: $email');
-                print('Telepon: $tlpn');
-                print('Jenis Kelamin: $jenisKel');
-                print('Kota Terdekat: $kota');
-                print('Persetujuan: $setuju');
+                print('Nama: $nama'); // mencetak nilai nama
+                print('Email: $email'); // mencetak nilai email
+                print('Telepon: $tlpn'); // mencetak nilai telepon
+                print('Jenis Kelamin: $jenisKel'); // mencetak nilai jenis kelamin
+                print('Kota Terdekat: $kota'); // mencetak nilai kota terdekat
+                print('Persetujuan: $setuju'); // mencetak nilai persetujuan
 
                 // // kita dapat tambahkan logika pada form  di dalam sini
                 // // pada contoh ini kita menampilkan data pada Dialog
@@ -234,7 +234,7 @@ class _UserProfileFormState extends State<Register02> { // class _UserProfileFor
                 //   },
                 // );
               },
-              child: Text('Daftar'),
+              child: Text('Daftar'), // child digunakan untuk konten dari tombol
             ),
           ],
         ),
